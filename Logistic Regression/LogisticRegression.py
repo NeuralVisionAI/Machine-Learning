6ed85276d1e2f7827dde8ad60b33e271d40fec42
Dhruv Rao <dhruv.rao1903@gmail.com>
2026-01-17 15:31:14 -0500

    initial commit

--- Logistic Regression/LogisticRegression.py
+++ Logistic Regression/LogisticRegression.py
@@ -5,7 +5,6 @@ def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
 class LogisticRegression:
-
     def __init__(self, lr = 0.001, n_iters = 1000):
         self.lr = lr
         self.n_iters = n_iters
