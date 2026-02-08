85d86d96a8a5993abee1e2e366bc314f018d620d
Vision <dhruv.rao1903@gmail.com>
2026-02-08 18:44:55 -0500

    Delete SVM directory

--- SVM/train.py
+++
@@ -1,8 +0,0 @@
-print("This  is the main branch file.")
-
-print("This is an addition by feature2 branch and i am created after feature was deleted.")
-
-
-
-print("this is and addition by main branch line7 to address a merge conflict.")
-print("this is addition in feature2 line5 for merge conflict addressing.")
