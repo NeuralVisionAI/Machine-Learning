3b9cee794ebd7133c60db23e2452d9b649e3f247
Dhruv Rao <dhruv.rao1903@gmail.com>
2026-02-08 18:18:28 -0500

    added line5 for merge conflict

--- SVM/train.py
+++ SVM/train.py
@@ -1,3 +1,5 @@
 print("This  is the main branch file.")
 
 print("This is an addition by feature2 branch and i am created after feature was deleted.")
+
+print("this is addition in feature2 line5 for merge conflict addressing.")
